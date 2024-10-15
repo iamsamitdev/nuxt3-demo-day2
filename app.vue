42520861d6721c0236c3a0b6517a00cbe2a9203a
Samit Koyom <samitkoyom@gmail.com>
2024-10-16 00:31:17 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import '~/assets/css/app.css'
+</script>
+<template>
+
+  <main>
+    <div class="menu">
+      <ul>
+        <li>
+          <NuxtLink to="/">/</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about">/about</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/contact">/contact</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/users/samit?type=admin&page=1">/users/samit?type=admin&page=1</NuxtLink>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <NuxtPage />
+    </div>
+  </main>
+  
+</template>
